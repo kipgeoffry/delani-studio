79b886fdc5fd7fa06ac61ae442519c64593e6f4d
kipgeoffry <kipgeoffry@gmail.com>
2023-04-07 23:21:06 +0300

    adding what we do section description

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    // display design description
+    $('#design-sec').click(function(){
+        $('#design-desc').toggle();
+    });
+    // display development description
+    $('#dev-sec').click(function(){
+        $('#dev-desc').toggle();
+    });
+    // display product management description
+    $('#prod-dev-sec').click(function(){
+        $('#prod-dev-desc').toggle();
+    });
+
+
+
+});
