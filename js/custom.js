2eed915145abbf29986633f4bd8fe672e88da86d
kipgeoffry <kipgeoffry@gmail.com>
2023-04-07 23:36:34 +0300

    adding js file to show and hide decsription when what we do icons is clicked

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
     // display design description
-    $('#design-sec').click(function(){
-        $('#design-desc').toggle();
-    });
-    // display development description
-    $('#dev-sec').click(function(){
-        $('#dev-desc').toggle();
-    });
-    // display product management description
-    $('#prod-dev-sec').click(function(){
-        $('#prod-dev-desc').toggle();
-    });
+    // $('#design-sec').click(function(){
+    //     $('#design-desc').toggle();
+    // });
+    // // display development description
+    // $('#dev-sec').click(function(){
+    //     $('#dev-desc').toggle();
+    // });
+    // // display product management description
+    // $('#prod-dev-sec').click(function(){
+    //     $('#prod-dev-desc').toggle();
+    // });
+
+    $('.wwd').click(function() {
+        // hide all other descriptions
+        $('.what-we-do').not($(this).next()).hide();
+        // toggle clicked description
+        $(this).next().toggle();
+      });
 
 
 
