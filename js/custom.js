543ab75539e98c217b84e79a2dc4f7fa9ea08b90
kipgeoffry <kipgeoffry@gmail.com>
2023-04-08 11:10:55 +0300

    updated hover effects on portfolio section

--- js/custom.js
+++ js/custom.js
@@ -12,4 +12,54 @@ $(document).ready(function(){
         $('#prod-dev-desc').toggle();
     });
 
+    // hover on portfolio
+    $("#work4").mouseover(function(){
+        $("#hover_design1").show();
+      }).mouseout(function(){
+        $("#hover_design1").hide();
+      });
+
+      $("#work3").mouseover(function(){
+        $("#hover_design2").show();
+      }).mouseout(function(){
+        $("#hover_design2").hide();
+      });
+
+      $("#work2").mouseover(function(){
+        $("#hover_design3").show();
+      }).mouseout(function(){
+        $("#hover_design3").hide();
+      });
+
+      $("#work1").mouseover(function(){
+        $("#hover_design4").show();
+      }).mouseout(function(){
+        $("#hover_design4").hide();
+      });
+
+      $("#work5").mouseover(function(){
+        $("#hover_design5").show();
+      }).mouseout(function(){
+        $("#hover_design5").hide();
+      });
+
+      $("#work6").mouseover(function(){
+        $("#hover_design6").show();
+      }).mouseout(function(){
+        $("#hover_design6").hide();
+      });
+
+      $("#work7").mouseover(function(){
+        $("#hover_design7").show();
+      }).mouseout(function(){
+        $("#hover_design7").hide();
+      });
+
+      $("#work8").mouseover(function(){
+        $("#hover_design8").show();
+      }).mouseout(function(){
+        $("#hover_design8").hide();
+      });
+
+
 });
